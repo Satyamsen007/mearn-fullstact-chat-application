6fdd82b7ddd86edc2a935c0f93c336b786f3e144
Satyamsen007 <satyamsen280@gmail.com>
2025-05-29 10:34:53 +0530

    Some Changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,13 @@ import { connectDB } from "./lib/db.js";
 import errorHandler from "./middleware/errorHandler.middleware.js";
 import { app, server } from "./lib/socket.js";
 import path from "path";
+import { fileURLToPath } from "url";
 // Load environment variables before any other code
 dotenv.config();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5001;
 
@@ -37,8 +42,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
-    })
+    });
 }
+
 
 // Error handling middleware should be last
 app.use(errorHandler);
