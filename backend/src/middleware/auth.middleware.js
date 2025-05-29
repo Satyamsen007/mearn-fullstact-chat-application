e5427a718a8865ae46ff0b0610546c516a1f60ba
Satyamsen007 <satyamsen280@gmail.com>
2025-05-29 10:29:16 +0530

    Some Changes

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,22 +2,22 @@
 import jwt from "jsonwebtoken"
 import { asyncHandler } from "../helper/asyncHandler.js"
 import User from "../models/user.model.js"
-import ApiError from "../helper/apiError.js"
+import ApiError from "../helper/ApiError.js"
 
 
-export const isAuthenticated = asyncHandler(async(req, _, next) => {
+export const isAuthenticated = asyncHandler(async (req, _, next) => {
     try {
         const token = req.cookies?.token || req.header("Authorization")?.replace("Bearer ", "")
-        
+
         // console.log(token);
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
-    
+
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
 
-        const user = await User.findById({_id:decodedToken?.userId}).select("-password")
-    
+        const user = await User.findById({ _id: decodedToken?.userId }).select("-password")
+
         if (!user) {
             throw new ApiError(401, "Invalid JWt Token")
         }
@@ -26,5 +26,5 @@ export const isAuthenticated = asyncHandler(async(req, _, next) => {
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid JWT token")
     }
-    
+
 })
